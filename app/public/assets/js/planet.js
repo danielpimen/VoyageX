710c67d70219e241076de09678e9699c98780d9f
Daniel P <danielpimen92@gmail.com>
2018-03-21 21:14:16 -0400

    new file

---
+++ app/public/assets/js/planet.js
@@ -0,0 +1,16 @@
+console.log('sup doge');
+$('#selectBtn').click(function(){
+let userChoice= { 
+ shuttle : $('#exampleFormControlSelect1').val(),
+ hotel : $('#exampleFormControlSelect2').val()
+};
+
+console.log(userChoice);
+
+    $.post('/user/trip', userChoice, function(data) {
+         alert('Submitted');
+    }).then(function() {
+        console.log(userChoice);
+    });
+
+});
